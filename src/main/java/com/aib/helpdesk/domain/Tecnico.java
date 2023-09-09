f44b94d78818f3c4f3adbade917f124c3b03073b
edgarbispo <edgarbispo@hotmail.com>
2023-09-09 10:43:48 -0300

    Endpoint Create para Tecnico

--- src/main/java/com/aib/helpdesk/domain/Tecnico.java
+++ src/main/java/com/aib/helpdesk/domain/Tecnico.java
@@ -2,10 +2,12 @@ package com.aib.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
+import com.aib.helpdesk.domain.dtos.TecnicoDTO;
 import com.aib.helpdesk.domain.enums.Perfil;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -31,6 +33,18 @@ public class Tecnico extends Pessoa{
 		// TODO Auto-generated constructor stub
 	}
 
+	public Tecnico(TecnicoDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
+	}
+	
+	
 	public List<Chamado> getChamados() {
 		return chamados;
 	}

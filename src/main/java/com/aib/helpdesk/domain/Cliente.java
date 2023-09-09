d81ed362714e9f9fd8d8411a28f4c045ba92f8e3
edgarbispo <edgarbispo@hotmail.com>
2023-09-09 12:20:57 -0300

    CRUD para Cliente implementado

--- src/main/java/com/aib/helpdesk/domain/Cliente.java
+++ src/main/java/com/aib/helpdesk/domain/Cliente.java
@@ -2,10 +2,12 @@ package com.aib.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
+import com.aib.helpdesk.domain.dtos.ClienteDTO;
 import com.aib.helpdesk.domain.enums.Perfil;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -30,6 +32,18 @@ public class Cliente extends Pessoa {
 		// TODO Auto-generated constructor stub
 	}
 
+	public Cliente(ClienteDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
+	}
+	
+	
 	public List<Chamado> getChamados() {
 		return chamados;
 	}

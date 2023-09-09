0e0db35567f49719deb0eb19ac89167bd6852812
edgarbispo <edgarbispo@hotmail.com>
2023-09-09 09:16:37 -0300

    Protegendo aplicação contra serialização

--- src/main/java/com/aib/helpdesk/domain/Tecnico.java
+++ src/main/java/com/aib/helpdesk/domain/Tecnico.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.aib.helpdesk.domain.enums.Perfil;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Tecnico extends Pessoa{
@@ -14,6 +15,7 @@ public class Tecnico extends Pessoa{
 
 	private static final long serialVersionUID = 1L;
 	
+	@JsonIgnore
 	@OneToMany(mappedBy = "tecnico")
 	private List<Chamado> chamados = new ArrayList<>();
 

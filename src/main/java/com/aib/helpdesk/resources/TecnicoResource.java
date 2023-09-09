bc56bcf46c002570db54934e50909d06189b3a8e
edgarbispo <edgarbispo@hotmail.com>
2023-09-09 09:17:18 -0300

    Endpoint findById para Tecnico

---
+++ src/main/java/com/aib/helpdesk/resources/TecnicoResource.java
@@ -0,0 +1,28 @@
+package com.aib.helpdesk.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aib.helpdesk.domain.Tecnico;
+import com.aib.helpdesk.services.TecnicoService;
+
+@RestController
+@RequestMapping(value = "/tecnicos")
+public class TecnicoResource {
+	
+	//localhost:8080/tecnicos
+
+	@Autowired
+	private TecnicoService service;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Tecnico> finById(@PathVariable Integer id){
+		Tecnico obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+}
